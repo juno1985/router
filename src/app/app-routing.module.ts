7683da1e9ec2859663e4024f144d077fd83b18f1
Jun <wangzhen_tju@126.com>
2017-12-20 15:03:41 +0800

    添加404 not found路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { Code404Component } from './code404/code404.component';
 
 const routes: Routes = [
   /**
@@ -9,7 +10,9 @@ const routes: Routes = [
    * component: 路由组件
    */
   {path:'' , component: HomeComponent},
-  {path:'product', component: ProductComponent}
+  {path:'product', component: ProductComponent},
+  /**404一定放在路由配置最后,以上都找不到才会路由到它 */
+  {path: '**', component: Code404Component}
 ];
 
 @NgModule({
