80436953fcc83921f10418b5abe4248decedfe30
Jun <wangzhen_tju@126.com>
2017-12-20 21:58:30 +0800

    CanActivate进入页面前守卫

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { Code404Component } from './code404/code404.component';
 import { ProductDescComponent } from './product-desc/product-desc.component';
 import { SellerInfoComponent } from './seller-info/seller-info.component';
 import { ChatComponent } from './chat/chat.component';
+import { LoginGuard } from './guard/login.guard';
 
 const routes: Routes = [
   /**
@@ -17,14 +18,16 @@ const routes: Routes = [
   {path:'chat' , component: ChatComponent, outlet:'aux'},
   {path:'product/:id', component: ProductComponent,children:[
     {path:'' , component: ProductDescComponent},
-    {path:'seller/:id' , component: SellerInfoComponent}
-  ]},
+    {path:'seller/:id' , component: SellerInfoComponent}],
+    canActivate:[LoginGuard]
+  },
   /**404一定放在路由配置最后,以上都找不到才会路由到它 */
   {path: '**', component: Code404Component}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[LoginGuard]
 })
 export class AppRoutingModule { }
